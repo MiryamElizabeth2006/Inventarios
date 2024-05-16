beaca9f35057d76f350faf14a96289a6e2918693
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2024-05-16 14:21:01 -0500

    Reto 14 realizado agregar datos a la tabla historial

--- JAVA/inventarios/src/main/java/com/krakedev/inventarios/bdd/PedidoBDD.java
+++ JAVA/inventarios/src/main/java/com/krakedev/inventarios/bdd/PedidoBDD.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -21,49 +22,49 @@ public class PedidoBDD {
 		ResultSet rsClave = null;
 		PreparedStatement psDet = null;
 		int codigoCabecera = 0;
-		
-		Date fechaActual= new Date();
-		java.sql.Date fechaSQL=new java.sql.Date(fechaActual.getTime()); 
-		
+
+		Date fechaActual = new Date();
+		java.sql.Date fechaSQL = new java.sql.Date(fechaActual.getTime());
+
 		try {
 			con = ConexionBDD.obtenerConexion();
-			ps=con.prepareStatement("insert into cabecera_pedido(proveedor, fecha, estado) "
-					+ "values(?,?,?)", Statement.RETURN_GENERATED_KEYS);
+			ps = con.prepareStatement("insert into cabecera_pedido(proveedor, fecha, estado) " + "values(?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
 
 			ps.setString(1, pedido.getProveedor().getIdentificador());
 			ps.setDate(2, fechaSQL);
 			ps.setString(3, "S");
-			
+
 			ps.executeUpdate();
-			
+
 			rsClave = ps.getGeneratedKeys();
-			
-			if(rsClave.next()) {
-				codigoCabecera=rsClave.getInt(1);
+
+			if (rsClave.next()) {
+				codigoCabecera = rsClave.getInt(1);
 			}
 
 			ArrayList<DetallePedido> detallesPedido = pedido.getDetalles();
 			DetallePedido det;
-			for(int i = 0;i<detallesPedido.size();i++) {
+			for (int i = 0; i < detallesPedido.size(); i++) {
 				det = detallesPedido.get(i);
-			psDet = con.prepareStatement("insert into detalle_pedido(cabecera_pedido, producto, cantidad_solicitada,cantidad_recibida, subtotal) "
-					+ "values(?,?,?,?,?)");
-			
-			psDet.setInt(1, codigoCabecera);
-			psDet.setInt(2, det.getProducto().getCodigo());
-			psDet.setInt(3, det.getCantidadSolicitada());
-			psDet.setInt(4, 0);
-			BigDecimal pv =det.getProducto().getPrecioVenta();
-			BigDecimal cantidad = new BigDecimal(det.getCantidadSolicitada());
-			BigDecimal subtotal = pv.multiply(cantidad);
-			psDet.setBigDecimal(5, subtotal);
-			
-			psDet.executeUpdate();
-			
-			System.out.println(psDet);
+				psDet = con.prepareStatement(
+						"insert into detalle_pedido(cabecera_pedido, producto, cantidad_solicitada,cantidad_recibida, subtotal) "
+								+ "values(?,?,?,?,?)");
+
+				psDet.setInt(1, codigoCabecera);
+				psDet.setInt(2, det.getProducto().getCodigo());
+				psDet.setInt(3, det.getCantidadSolicitada());
+				psDet.setInt(4, 0);
+				BigDecimal pv = det.getProducto().getPrecioVenta();
+				BigDecimal cantidad = new BigDecimal(det.getCantidadSolicitada());
+				BigDecimal subtotal = pv.multiply(cantidad);
+				psDet.setBigDecimal(5, subtotal);
+
+				psDet.executeUpdate();
+
+				System.out.println(psDet);
 			}
-		
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new KrakeDevException("Error al insertar cliente. Detalle: " + e.getMessage());
@@ -79,12 +80,17 @@ public class PedidoBDD {
 			}
 		}
 	}
-	
-	//Modificar lo que se inserto en pedidios y detalle pedidos
+
+	// Modificar lo que se inserto en pedidios y detalle pedidos
 	public void modificar(Pedido pedido) throws KrakeDevException {
 		Connection con = null;
 		PreparedStatement ps = null;
 		PreparedStatement psDet = null;
+		PreparedStatement psHis=null;
+		Date fechaActual = new Date();
+		Timestamp fechaHoraActual = new Timestamp(fechaActual.getTime());
+
+
 		
 		try {
 			con = ConexionBDD.obtenerConexion();
@@ -111,15 +117,32 @@ public class PedidoBDD {
 				psDet.setInt(3, det.getCodigo());
 				
 				psDet.executeUpdate();	
+				
+				//Insertar el la tabla historial
+				
+				psHis = con.prepareStatement("INSERT INTO historial_stock( "
+						+ "	fecha_hora, referencia, producto, cantidad) "
+						+ "	VALUES (?, ?, ?, ?);",Statement.RETURN_GENERATED_KEYS);
+				
+				
+				psHis.setTimestamp(1,fechaHoraActual);
+				psHis.setString(2, "Pedido "+pedido.getCodigo());
+				psHis.setInt(3, det.getProducto().getCodigo());
+				psHis.setInt(4, det.getCantidadRecibida());
+				
+				psHis.executeUpdate();
+				
 			
 			System.out.println("Detalle de pedio agregado"+psDet);
-			}	
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new KrakeDevException("Error al insertar cliente. Detalle: " + e.getMessage());
-		} catch (KrakeDevException e) {
-			throw e;
-		} finally {
+			}}catch(SQLException e)
+	{
+		e.printStackTrace();
+		throw new KrakeDevException("Error al insertar cliente. Detalle: " + e.getMessage());
+	}catch(KrakeDevException e)
+	{
+		throw e;
+	}finally
+	{
 			if (con != null) {
 				try {
 					con.close();
@@ -128,5 +151,4 @@ public class PedidoBDD {
 				}
 			}
 		}
-	}
-}
+}}
